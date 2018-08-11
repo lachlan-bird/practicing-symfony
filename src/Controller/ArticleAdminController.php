23dc173fa2f4bc8126581d1b90fd77b1a587d0b6
Lachlan Bird <lachlan.bird88@gmail.com>
2018-08-11 18:20:20 +1000

    Start scaffolding out articles. Add Doctrine ORM

---
+++ src/Controller/ArticleAdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleAdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/article/new", name="article_admin")
+     */
+    public function new()
+    {
+        return new Response('Cool!');
+    }
+}
