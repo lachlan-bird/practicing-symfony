2ddabc196a8c52ff13c0b7a04c13801ac8239e57
Lachlan Bird <lachlan.bird88@gmail.com>
2018-08-12 16:40:53 +1000

    Remove N+1 query problem when getting all tags for an article

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -27,6 +27,8 @@ class ArticleRepository extends ServiceEntityRepository
     public function findAllPublishedByNewest()
     {
         return $this->addIsPublishedQueryBuilder()
+            ->leftJoin('a.tags', 't')
+            ->addSelect('t')
             ->andWhere('a.publishedAt IS NOT NULL')
             ->orderBy('a.publishedAt', 'DESC')
             ->getQuery()
