170168a03c1308967da3e209fd62ec931f3c8515
Lachlan Bird <lachlan.bird88@gmail.com>
2018-08-12 11:35:04 +1000

    Add table of comments to admin section

---
+++ src/Controller/CommentAdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CommentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentAdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/comment", name="comment_admin")
+     */
+    public function index(CommentRepository $commentRepository)
+    {
+        return $this->render('comment_admin/index.html.twig', [
+            'comments' => $commentRepository->findBy([], ['createdAt' => 'DESC'])
+        ]);
+    }
+}
